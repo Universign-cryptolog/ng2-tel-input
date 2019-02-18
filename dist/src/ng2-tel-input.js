6a35a8da47a1a159bc307be4b293de5f65e6c16a
chaaben <mohamed.chaaben@universign.com>
2019-02-18 14:31:56 +0100

    [Upgrade to Angular 7]

--- dist/src/ng2-tel-input.js
+++ dist/src/ng2-tel-input.js
@@ -1,3 +1,12 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import * as $ from 'jquery';
 import 'intl-tel-input';
@@ -41,23 +50,38 @@ var Ng2TelInput = /** @class */ (function () {
     Ng2TelInput.prototype.setCountry = function (country) {
         this.ngTelInput.intlTelInput('setCountry', country);
     };
-    Ng2TelInput.decorators = [
-        { type: Directive, args: [{
-                    selector: '[ng2TelInput]',
-                },] },
-    ];
-    /** @nocollapse */
-    Ng2TelInput.ctorParameters = function () { return [
-        { type: ElementRef, },
-    ]; };
-    Ng2TelInput.propDecorators = {
-        "ng2TelInputOptions": [{ type: Input, args: ['ng2TelInputOptions',] },],
-        "hasError": [{ type: Output, args: ['hasError',] },],
-        "ng2TelOutput": [{ type: Output, args: ['ng2TelOutput',] },],
-        "countryChange": [{ type: Output, args: ['countryChange',] },],
-        "intlTelInputObject": [{ type: Output, args: ['intlTelInputObject',] },],
-        "onKeyup": [{ type: HostListener, args: ['keyup',] },],
-    };
+    __decorate([
+        Input('ng2TelInputOptions'),
+        __metadata("design:type", Object)
+    ], Ng2TelInput.prototype, "ng2TelInputOptions", void 0);
+    __decorate([
+        Output('hasError'),
+        __metadata("design:type", EventEmitter)
+    ], Ng2TelInput.prototype, "hasError", void 0);
+    __decorate([
+        Output('ng2TelOutput'),
+        __metadata("design:type", EventEmitter)
+    ], Ng2TelInput.prototype, "ng2TelOutput", void 0);
+    __decorate([
+        Output('countryChange'),
+        __metadata("design:type", EventEmitter)
+    ], Ng2TelInput.prototype, "countryChange", void 0);
+    __decorate([
+        Output('intlTelInputObject'),
+        __metadata("design:type", EventEmitter)
+    ], Ng2TelInput.prototype, "intlTelInputObject", void 0);
+    __decorate([
+        HostListener('keyup'),
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", []),
+        __metadata("design:returntype", void 0)
+    ], Ng2TelInput.prototype, "onKeyup", null);
+    Ng2TelInput = __decorate([
+        Directive({
+            selector: '[ng2TelInput]',
+        }),
+        __metadata("design:paramtypes", [ElementRef])
+    ], Ng2TelInput);
     return Ng2TelInput;
 }());
 export { Ng2TelInput };
--- dist/src/ng2-tel-input.module.js
+++ dist/src/ng2-tel-input.module.js
@@ -1,20 +1,28 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 import { NgModule } from '@angular/core';
 import { Ng2TelInput } from './ng2-tel-input';
 var Ng2TelInputModule = /** @class */ (function () {
     function Ng2TelInputModule() {
     }
+    Ng2TelInputModule_1 = Ng2TelInputModule;
     Ng2TelInputModule.forRoot = function () {
         return {
-            ngModule: Ng2TelInputModule,
+            ngModule: Ng2TelInputModule_1,
             providers: []
         };
     };
-    Ng2TelInputModule.decorators = [
-        { type: NgModule, args: [{
-                    declarations: [Ng2TelInput],
-                    exports: [Ng2TelInput]
-                },] },
-    ];
+    var Ng2TelInputModule_1;
+    Ng2TelInputModule = Ng2TelInputModule_1 = __decorate([
+        NgModule({
+            declarations: [Ng2TelInput],
+            exports: [Ng2TelInput]
+        })
+    ], Ng2TelInputModule);
     return Ng2TelInputModule;
 }());
 export { Ng2TelInputModule };
